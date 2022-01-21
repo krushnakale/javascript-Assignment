b7cdef8f313b8bbb0c9e3d8a3e207f35a894e195
krushna <krishnakale8565@gmail.com>
2022-01-21 14:25:11 +0530

    modified all code

---
+++ hello-user.js
@@ -0,0 +1,7 @@
+function handleButtonClick(){
+    username = document.getElementById("username").value
+    document.getElementById("heading").innerHTML = "welcome to our dashbaord " + username + "!";
+ 
+    window.alert(welcomeTo);
+
+}
