d1639475e74e9f7667efa52d1fbf4291cd6e5115
krushna <krishnakale8565@gmail.com>
2022-01-21 15:17:14 +0530

    change all code

--- email-form.js
+++ email-form.js
@@ -1,3 +1,9 @@
-<script>
-    document.getElementById('heading').innerHTML = "log in form";
-</script>
+
+// the following statement executes as soon as the script file referenced by the scripts tag in html file loads in the browser .
+// docment.getElementById("heading").innerHTML = "hello world";
+
+// to have things work on button click, we do not want these to run when browser loads the script
+// it should executes some time later when buttons gets clicked;
+function handleButtonClick() {
+    document.getElementById("")
+}
