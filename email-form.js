166d4a74e61d25aaf3fb72a6edb3f3e8d41e4d1f
krushna <krishnakale8565@gmail.com>
2022-02-02 07:50:02 +0530

    change the head section

--- email-form.js
+++ email-form.js
@@ -1,9 +1,6 @@
 
-// the following statement executes as soon as the script file referenced by the scripts tag in html file loads in the browser .
-// docment.getElementById("heading").innerHTML = "hello world";
-
-// to have things work on button click, we do not want these to run when browser loads the script
-// it should executes some time later when buttons gets clicked;
 function handleButtonClick() {
-    document.getElementById("")
+   send = document.getElementById("send").value
+   document.getElementById("send").innerHTML = "subject" + send + "subject";
+   
 }
